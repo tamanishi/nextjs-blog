ea8bf10cc201bdf42f8b3bcfc0a4b1aa4a7fb52f
Masayuki Sunahara <tamanishi@gmail.com>
2021-01-31 20:13:47 +0900

    Implement getStaticPaths

---
+++ pages/posts/[id].js
@@ -0,0 +1,12 @@
+import Layout from '../../components/layout'
+
+export async function getStaticPaths() {
+    const paths = getAllPostIds()
+    return {
+        paths,
+        fallback: false
+    }
+}
+export default function Post() {
+    return <Layout>...</Layout>
+}
